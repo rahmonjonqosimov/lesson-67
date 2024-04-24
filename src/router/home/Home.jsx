88c7fe619b84fce120f33a03c655202c0989b83d
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-04-24 21:25:30 +0500

    asdfghjkl

--- src/router/home/Home.jsx
+++ src/router/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 1 }}
     >
       <Hero />
       <Latest />
